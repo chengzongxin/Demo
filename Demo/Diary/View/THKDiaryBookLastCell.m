b53c0d54b422e92cdb69ed6babd07da19c4406c0
Joe.cheng <joe.cheng@corp.to8to.com>
2021-08-27 10:54:44 +0800

    求更新

--- Demo/Diary/View/THKDiaryBookLastCell.m
+++ Demo/Diary/View/THKDiaryBookLastCell.m
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) UILabel *contentLabel;
 @property (nonatomic, strong) THKDiaryCircleView *circleView;
 @property (nonatomic, strong) UIView *lineView;
+@property (nonatomic, strong) UIButton *updateButton;
 @end
 
 @implementation THKDiaryBookLastCell
@@ -39,6 +40,7 @@
     [self.contentView addSubview:self.lineView];
     [self.contentView addSubview:self.circleView];
     [self.contentView addSubview:self.contentLabel];
+    [self.contentView addSubview:self.updateButton];
     
     [self.lineView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.circleView.mas_centerX);
@@ -58,7 +60,73 @@
         make.right.mas_equalTo(-kDiaryContentInset.right);
     }];
     
+    [self.updateButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.contentLabel.mas_bottom).offset(50);
+        make.centerX.equalTo(self.contentView);
+        make.size.mas_equalTo(CGSizeMake(140, 50));
+    }];
+     
     [self.contentLabel tmui_setAttributesString:@"屋主还在为自己的家努力，你可以提醒TA继续更新～" lineSpacing:6];
+    
+}
+
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    
+//    [self addParticleEffect];
+}
+
+- (void)updateButtonClick:(UIButton *)btn{
+    [self addParticleEffect:CGPointMake(btn.centerX, btn.y)];
+}
+
+- (void)addParticleEffect:(CGPoint)point{
+    [self removeParticleEffect];
+    // 1.创建发射器
+    CAEmitterLayer *emitter = [[CAEmitterLayer alloc] init];
+    // 2.发射器位置
+    emitter.emitterPosition = point;
+    // 3.开启三维效果
+    emitter.preservesDepth = true;
+    
+    NSMutableArray *cells = [NSMutableArray array];
+    for (int i = 0; i < 10; i++) {
+        // 4.设置 Cell(对应其中一个粒子)
+        // 4.0.创建粒子
+        CAEmitterCell *cell = [[CAEmitterCell alloc] init];
+        // 4.1.每秒发射粒子数
+        cell.birthRate = arc4random_uniform(4) + 3;
+        // 4.2.粒子存活时间
+        cell.lifetime = 5;
+        cell.lifetimeRange = 2.5;
+        // 4.3.缩放比例
+        cell.scale = 0.7;
+        cell.scaleRange = 0.3;
+        // 4.4.粒子发射方向
+        cell.emissionLongitude = -M_PI_2;
+        cell.emissionRange = M_PI_4 * 0.6;
+        // 4.5.粒子速度
+        cell.velocity = 100;
+        cell.velocityRange = 50;
+        // 4.6.旋转速度
+        cell.spin = M_PI_2;
+        // 4.7.粒子内容
+        cell.contents = (__bridge id _Nullable)([UIImage imageNamed:@"icon_crown_yellow"].CGImage);
+        [cells addObject:cell];;
+    }
+    // 5.将粒子添加到发射器中
+    emitter.emitterCells = cells;
+    [self.layer addSublayer:emitter];
+}
+
+- (void)removeParticleEffect{
+    NSArray *emitters = [self.layer.sublayers tmui_filter:^BOOL(__kindof CALayer * _Nonnull item) {
+        return [item isKindOfClass:CAEmitterLayer.class];
+    }];
+    
+    if (emitters.count) {
+        [emitters makeObjectsPerformSelector:@selector(removeFromSuperlayer)];
+    }
 }
 
 - (void)bindViewModel:(THKDiaryBookCellVM *)viewModel{
@@ -100,4 +168,22 @@
     }
     return _contentLabel;
 }
+
+- (UIButton *)updateButton{
+    if (!_updateButton) {
+        _updateButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 140, 50)];
+        _updateButton.backgroundColor = UIColor.whiteColor;
+        _updateButton.tmui_image = UIImageMake(@"diary_bixin_icon");
+        _updateButton.tmui_text = @"求更新";
+        _updateButton.layer.cornerRadius = 25;
+        _updateButton.layer.borderColor = UIColorHex(F6F8F6).CGColor;
+        _updateButton.layer.borderWidth = 0.5;
+        _updateButton.tmui_font = UIFont(16);
+        _updateButton.tmui_titleColor = UIColorHex(333533);
+        [_updateButton.layer tmui_setLayerShadow:UIColor.blackColor offset:CGSizeMake(0, 5) alpha:0.05 radius:5 spread:0];
+        [_updateButton tmui_addTarget:self action:@selector(updateButtonClick:)];
+    }
+    return _updateButton;
+}
+
 @end
