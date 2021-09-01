574ec0e060542a473998fad81a5ba716cc173e46
Joe.cheng <joe.cheng@corp.to8to.com>
2021-09-01 10:47:37 +0800

    update

--- Demo/THKExpandLabel.m
+++ Demo/THKExpandLabel.m
@@ -87,79 +87,72 @@
 - (NSAttributedString *)contentAttrString{
     if (_contentAttrString == nil) {
         CGFloat contentWidth = _maxWidth;
-        NSString *tagStr = self.tagStr?:@"";
-        NSString *contentStr = [self.contentStr tmui_trim];
-        
-        NSString *allStr = [NSString stringWithFormat:@"%@%@",tagStr,contentStr];
-        
+        NSString *tagStr = self.tagStr;
+        NSDictionary *tagAttrDict = self.tagAttrDict;
+        NSString *contentStr = self.contentStr;
+        NSDictionary *contentAttrDict = self.contentAttrDict;
         UIFont *font = _preferFont;
-//        if (!font) {
-//            font = [attributedText tmui_font];
-//        }
-        if (!font) {
-            font = self.font?:UIFont(17);
-        }
         
-        if (_maxWidth == 0) {
-            [self layoutIfNeeded];
-            _maxWidth = self.width;
-        }
         
-        NSMutableAttributedString *calcAttr = [[NSMutableAttributedString alloc] initWithString:allStr attributes:self.contentAttrDict];
-        [calcAttr addAttributes:self.tagAttrDict range:[allStr rangeOfString:tagStr]];
+        NSMutableAttributedString *calcAttr = [[NSMutableAttributedString alloc] init];
+        [calcAttr appendAttributedString:[[NSAttributedString alloc] initWithString:tagStr attributes:tagAttrDict]];
+        [calcAttr appendAttributedString:[[NSAttributedString alloc] initWithString:contentStr attributes:contentAttrDict]];
         
         NSArray *lines = [self getLinesForAttrStr:calcAttr maxWidth:_maxWidth];
         
-        NSLog(@"111 %@",lines);
-//        [allStr tmui_linesArrayForFont:nil maxWidth:0];
-        if (self.numberOfLines > 0) {
-            // 只显示3行文字
-            NSInteger limitLine = self.numberOfLines;
-            NSString *dotStr = @"...     ";
-            if (lines.count > limitLine) {
-                
-                NSArray *lineStrs = [self getLineTextsFromLines:lines str:calcAttr];
-                self.isFold = YES;
-                NSMutableString *foldStr = [NSMutableString string];
-                for (int i = 0; i < limitLine; i++) {
-                    NSString *lineStr = lineStrs[i];
-                    if (i == limitLine - 1) {
-                        // 最后一行，铺满需要截取
-                        // 最后一行宽度
-                        CGFloat width = [lineStr tmui_widthForFont:font];
-                        // 拼接文本
-                        NSString *appendStr = [NSString stringWithFormat:@"%@%@",dotStr,self.foldBtn.tmui_text];
-                        CGFloat appendWidth = [appendStr tmui_sizeWithFont:_preferFont width:_maxWidth].width;
-                        CGFloat padding = 10;
-                        CGFloat addtionGap = 4;
-                        if (width > contentWidth - appendWidth - padding) {
-                            lineStr = [lineStr substringToIndex:lineStr.length - appendStr.tmui_lengthWhenCountingNonASCIICharacterAsTwo - addtionGap];
-                        }
-                        lineStr = [[lineStr tmui_trim] stringByAppendingString:dotStr];
-                        
-                    }
-                    [foldStr appendString:lineStr];
-                }
-                allStr = [foldStr tmui_trim]; // 去掉尾部换行符
+        NSMutableAttributedString* (^setLineGap)(NSMutableAttributedString*) = ^NSMutableAttributedString *(NSMutableAttributedString *attr) {
+            if (lines.count >= 1) {
+                NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+                paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+                [paragraphStyle setLineSpacing:self.lineGap];// 调整行间距
+//                [attr tmui_setAttributes:@{NSParagraphStyleAttributeName:paragraphStyle}];
+                [attr addAttributes:@{NSParagraphStyleAttributeName:paragraphStyle} range:NSMakeRange(0, attr.string.length-1)];
             }
-        }else{
+            return attr;
+        };
+        
+        calcAttr = setLineGap(calcAttr);
+        
+        NSInteger limitLine = self.numberOfLines;
+        if (limitLine <= 0 || limitLine >= lines.count) {
+            // 未折叠
             self.isFold = NO;
+            _contentAttrString = calcAttr;
+        }else{
+            // 当前是折叠状态，需要在最后一行拼接。。。
+            self.isFold = YES;
+            NSString *dotStr = @"...     ";
+            NSArray *lineStrs = [self getLineTextsFromLines:lines str:calcAttr];
+            NSMutableString *foldStr = [NSMutableString string];
+            for (int i = 0; i < limitLine; i++) {
+                NSString *lineStr = lineStrs[i];
+                if (i == limitLine - 1) {
+                    // 最后一行，铺满需要截取
+                    // 最后一行宽度
+                    CGFloat width = [lineStr tmui_widthForFont:font];
+                    // 拼接文本
+                    NSString *appendStr = [NSString stringWithFormat:@"%@%@",dotStr,self.foldBtn.tmui_text];
+                    CGFloat appendWidth = [appendStr tmui_sizeWithFont:_preferFont width:_maxWidth].width;
+                    CGFloat padding = 10;
+                    CGFloat addtionGap = 4;
+                    if (width > contentWidth - appendWidth - padding) {
+                        lineStr = [lineStr substringToIndex:lineStr.length - appendStr.tmui_lengthWhenCountingNonASCIICharacterAsTwo - addtionGap];
+                    }
+                    lineStr = [lineStr stringByAppendingString:dotStr];
+                }
+                [foldStr appendString:lineStr];
+            }
+            
+            NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:foldStr attributes:self.contentAttrDict];
+            NSRange tagR = [foldStr rangeOfString:tagStr];
+            [attr addAttributes:self.tagAttrDict range:tagR];
+            setLineGap(attr);
+            _contentAttrString = attr;
         }
-        
-        NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:allStr attributes:self.contentAttrDict];
-        [attr addAttributes:self.tagAttrDict range:[allStr rangeOfString:tagStr]];
-        
-        if (lines.count >= 1) {
-            NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-            paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
-            [paragraphStyle setLineSpacing:self.lineGap];// 调整行间距
-            [attr tmui_setAttributes:@{NSParagraphStyleAttributeName:paragraphStyle}];
-        }
-        
-        _contentAttrString = attr;
     }
     return _contentAttrString;
 }
+
 
 /// 获取富文本占据行数 CTLine
 - (NSArray *)getLinesForAttrStr:(NSAttributedString *)attrStr maxWidth:(CGFloat)maxWidth{
