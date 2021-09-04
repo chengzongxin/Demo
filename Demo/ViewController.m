0c74c0d58ec8ee304279f5babd379f95f3e736a3
Joe.cheng <joe.cheng@corp.to8to.com>
2021-09-04 17:16:44 +0800

    update

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -24,10 +24,18 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self animate1];
+    [self animate1:1];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self animate1:2];
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self animate1:3];
+    });
 }
 
-- (void)animate1{
+- (void)animate1:(NSInteger)animateType{
     UIImage *img = [UIImage imageNamed:@"diary_heart_fly"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:img];
     imageView.frame = CGRectMake(-1000, -1000, img.size.width, img.size.height);
@@ -39,7 +47,22 @@
     CGPoint startPoint = CGPointMake(200, 700);
     CGPoint endPoint = CGPointMake(100, 80);
     
-    CAAnimation *animate = [self heartFlyAnimate1:startPoint endPoint:endPoint];
+    CAAnimation *animate;
+    
+    switch (animateType) {
+        case 1:
+            animate = [self heartFlyAnimate1:startPoint endPoint:endPoint];
+            break;
+        case 2:
+            animate = [self heartFlyAnimate2:startPoint endPoint:endPoint];
+            break;
+        case 3:
+            animate = [self heartFlyAnimate3:startPoint endPoint:endPoint];
+            break;
+            
+        default:
+            break;
+    }
     
     [imageView.layer addAnimation:animate forKey:nil];
     
@@ -52,7 +75,7 @@
 
 
 - (CAAnimation *)heartFlyAnimate1:(CGPoint)startPoint endPoint:(CGPoint)endPoint {
-    CGPoint controlPoint = CGPointMake(startPoint.x + 20, (startPoint.y - endPoint.y)/2);
+    CGPoint controlPoint = CGPointMake(startPoint.x + 200, (startPoint.y - endPoint.y)/2);
     CGFloat duration = 2.0;
     
     // 位置
@@ -128,12 +151,161 @@
     
     CAAnimationGroup *animGroup = [CAAnimationGroup animation];
     animGroup.animations = @[scaleAnim1, scaleAnim2, rotationAnim1, rotationAnim2, opacity1Anim, opacity2Anim, positionAnim];
-    animGroup.duration = 3;
+    animGroup.duration = duration;
     positionAnim.removedOnCompletion = YES;
     
     return animGroup;
 }
 
+- (CAAnimation *)heartFlyAnimate2:(CGPoint)startPoint endPoint:(CGPoint)endPoint {
+    CGPoint controlPoint = CGPointMake(startPoint.x - 200, startPoint.y);
+    CGFloat duration = 2.0;
+    
+    // 位置
+    CAKeyframeAnimation *positionAnim =[CAKeyframeAnimation animationWithKeyPath:@"position"];
+    positionAnim.beginTime = 0;
+    positionAnim.duration = duration;
+    positionAnim.removedOnCompletion = YES;
+    positionAnim.fillMode = kCAFillModeRemoved;
+    positionAnim.repeatCount = 0;
+    positionAnim.calculationMode = kCAAnimationCubicPaced;
+    CGMutablePathRef curvedPath = CGPathCreateMutable();
+    CGPoint point0 = startPoint;
+    CGPoint point1 = endPoint;
+    CGPathMoveToPoint(curvedPath, NULL, point0.x, point0.y);
+    CGPathAddQuadCurveToPoint(curvedPath, NULL, controlPoint.x, controlPoint.y, point1.x, point1.y);
+    positionAnim.path = curvedPath;
+    
+    CGPathRelease(curvedPath);
+    
+    // 透明度变化
+    //因视图设置了alpha为0，为了一开始能正常显示出来，这里加一个固定stayAlpha1Sec秒，alpha为1的动画(仅仅是为了在前stayAlpha1Sec秒内视图能正常显示出来)
+    float stayAlpha1Sec = 1;
+    CABasicAnimation *opacity1Anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacity1Anim.fromValue = [NSNumber numberWithFloat:0.1];
+    opacity1Anim.toValue = [NSNumber numberWithFloat:1.0];
+    opacity1Anim.removedOnCompletion = NO;
+    opacity1Anim.beginTime = 0;
+    opacity1Anim.duration = stayAlpha1Sec;
+    
+    //正常显示stayAlpha1Sec秒后，再渐变alpha消失
+    CABasicAnimation *opacity2Anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacity2Anim.fromValue = [NSNumber numberWithFloat:1.0];
+    opacity2Anim.toValue = [NSNumber numberWithFloat:0];
+    opacity2Anim.removedOnCompletion = NO;
+    opacity2Anim.beginTime = stayAlpha1Sec;
+    opacity2Anim.duration = duration - stayAlpha1Sec;
+    
+    // 比例
+    float stayScale1Sec = 1;
+    CABasicAnimation *scaleAnim1 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnim1.fromValue = [NSNumber numberWithFloat:0.1];
+    scaleAnim1.toValue = [NSNumber numberWithFloat:1];
+    scaleAnim1.removedOnCompletion = NO;
+    scaleAnim1.fillMode = kCAFillModeForwards;
+    scaleAnim1.beginTime = 0;
+    scaleAnim1.duration = stayScale1Sec;
+    
+    CABasicAnimation *scaleAnim2 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnim2.fromValue = [NSNumber numberWithFloat:1];
+    scaleAnim2.toValue = [NSNumber numberWithFloat:0.2];
+    scaleAnim2.removedOnCompletion = NO;
+    scaleAnim2.fillMode = kCAFillModeForwards;
+    scaleAnim2.beginTime = duration - stayScale1Sec;
+    scaleAnim2.duration = duration - stayScale1Sec;
+    
+    CAAnimationGroup *animGroup = [CAAnimationGroup animation];
+    animGroup.animations = @[scaleAnim1, scaleAnim2, opacity1Anim, opacity2Anim, positionAnim];
+    animGroup.duration = duration;
+    positionAnim.removedOnCompletion = YES;
+    
+    return animGroup;
+}
+
+- (CAAnimation *)heartFlyAnimate3:(CGPoint)startPoint endPoint:(CGPoint)endPoint {
+    CGPoint controlPoint0 = CGPointMake(startPoint.x, (startPoint.y + endPoint.y) / 2);
+    CGPoint controlPoint1 = CGPointMake((startPoint.x + endPoint.x) /2, endPoint.y - 50);
+    CGFloat duration = 2.0;
+    
+    // 位置
+    CAKeyframeAnimation *positionAnim =[CAKeyframeAnimation animationWithKeyPath:@"position"];
+    positionAnim.beginTime = 0;
+    positionAnim.duration = duration;
+    positionAnim.removedOnCompletion = YES;
+    positionAnim.fillMode = kCAFillModeRemoved;
+    positionAnim.repeatCount = 0;
+    positionAnim.calculationMode = kCAAnimationCubicPaced;
+    CGMutablePathRef curvedPath = CGPathCreateMutable();
+    CGPoint point0 = startPoint;
+    CGPoint point1 = endPoint;
+    CGPathMoveToPoint(curvedPath, NULL, point0.x, point0.y);
+    CGPathAddQuadCurveToPoint(curvedPath, NULL, controlPoint0.x, controlPoint0.y, point0.x, point1.y);
+    CGPathAddQuadCurveToPoint(curvedPath, NULL, controlPoint1.x, controlPoint1.y, point1.x, point1.y);
+    positionAnim.path = curvedPath;
+    
+    CGPathRelease(curvedPath);
+    
+    // 透明度变化
+    //因视图设置了alpha为0，为了一开始能正常显示出来，这里加一个固定stayAlpha1Sec秒，alpha为1的动画(仅仅是为了在前stayAlpha1Sec秒内视图能正常显示出来)
+    float stayAlpha1Sec = 1;
+    CABasicAnimation *opacity1Anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacity1Anim.fromValue = [NSNumber numberWithFloat:0.1];
+    opacity1Anim.toValue = [NSNumber numberWithFloat:1.0];
+    opacity1Anim.removedOnCompletion = NO;
+    opacity1Anim.beginTime = 0;
+    opacity1Anim.duration = stayAlpha1Sec;
+    
+    //正常显示stayAlpha1Sec秒后，再渐变alpha消失
+    CABasicAnimation *opacity2Anim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacity2Anim.fromValue = [NSNumber numberWithFloat:1.0];
+    opacity2Anim.toValue = [NSNumber numberWithFloat:0];
+    opacity2Anim.removedOnCompletion = NO;
+    opacity2Anim.beginTime = stayAlpha1Sec;
+    opacity2Anim.duration = duration - stayAlpha1Sec;
+    
+    // 比例
+    float stayScale1Sec = 1;
+    CABasicAnimation *scaleAnim1 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnim1.fromValue = [NSNumber numberWithFloat:0.1];
+    scaleAnim1.toValue = [NSNumber numberWithFloat:1];
+    scaleAnim1.removedOnCompletion = NO;
+    scaleAnim1.fillMode = kCAFillModeForwards;
+    scaleAnim1.beginTime = 0;
+    scaleAnim1.duration = stayScale1Sec;
+    
+    CABasicAnimation *scaleAnim2 = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnim2.fromValue = [NSNumber numberWithFloat:1];
+    scaleAnim2.toValue = [NSNumber numberWithFloat:0.2];
+    scaleAnim2.removedOnCompletion = NO;
+    scaleAnim2.fillMode = kCAFillModeForwards;
+    scaleAnim2.beginTime = duration - stayScale1Sec;
+    scaleAnim2.duration = duration - stayScale1Sec;
+    
+    // 旋转
+    float stayRotation1Sec = 1;
+    CABasicAnimation *rotationAnim1 = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    rotationAnim1.fromValue = [NSNumber numberWithFloat:-0.5 * M_PI];
+    rotationAnim1.toValue = [NSNumber numberWithFloat:0];
+    rotationAnim1.removedOnCompletion = NO;
+    rotationAnim1.fillMode = kCAFillModeForwards;
+    rotationAnim1.beginTime = 0;
+    rotationAnim1.duration = stayRotation1Sec;
+    
+    CABasicAnimation *rotationAnim2 = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    rotationAnim2.fromValue = [NSNumber numberWithFloat:0];
+    rotationAnim2.toValue = [NSNumber numberWithFloat:0.3 * M_PI];
+    rotationAnim2.removedOnCompletion = NO;
+    rotationAnim2.fillMode = kCAFillModeForwards;
+    rotationAnim2.beginTime = duration - stayRotation1Sec;
+    rotationAnim2.duration = duration - stayRotation1Sec;
+    
+    CAAnimationGroup *animGroup = [CAAnimationGroup animation];
+    animGroup.animations = @[scaleAnim1, scaleAnim2, opacity1Anim, opacity2Anim, rotationAnim1, rotationAnim2,positionAnim];
+    animGroup.duration = duration;
+    positionAnim.removedOnCompletion = YES;
+    
+    return animGroup;
+}
 
 
 - (void)test{
