55f3adc9876c4d74c30db8d888c1cd812e4fb19c
Joe.cheng <joe.cheng@corp.to8to.com>
2022-08-12 10:04:09 +0800

    扬声器播放

--- Demo/HomeSection/OnlineDesign/Recorder/THKRecordTool.m
+++ Demo/HomeSection/OnlineDesign/Recorder/THKRecordTool.m
@@ -30,19 +30,36 @@ SHARED_INSTANCE_FOR_CLASS;
 }
 
 - (void)setupRecorder{
+    //----------------AVAudioRecorder----------------
+    // 录音会话设置
+    NSError *errorSession = nil;
+    AVAudioSession * audioSession = [AVAudioSession sharedInstance]; // 得到AVAudioSession单例对象
+    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error: &errorSession];// 设置类别,表示该应用同时支持播放和录音
+    [audioSession setActive:YES error: &errorSession];// 启动音频会话管理,此时会阻断后台音乐的播放.
+    
+    // 设置成扬声器播放
+    UInt32 doChangeDefault = 1;
+    AudioSessionSetProperty(kAudioSessionProperty_OverrideCategoryDefaultToSpeaker, sizeof(doChangeDefault), &doChangeDefault);
+    
     NSMutableDictionary *recordSettings = [[NSMutableDictionary alloc] init];
     [recordSettings setValue :[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey: AVFormatIDKey];
     [recordSettings setValue :[NSNumber numberWithFloat:11025.0] forKey: AVSampleRateKey];
     [recordSettings setValue :[NSNumber numberWithInt:2] forKey: AVNumberOfChannelsKey];
-    [recordSettings setValue:[NSNumber numberWithInt:AVAudioQualityMin] forKey:AVEncoderAudioQualityKey];
-    NSURL *url = [NSURL URLWithString:kRecordFilePath];
-    _audioRecorder = [[AVAudioRecorder alloc] initWithURL:url settings:recordSettings error:nil];
+    [recordSettings setValue :[NSNumber numberWithInt:AVAudioQualityHigh] forKey:AVEncoderAudioQualityKey];
+    _audioRecorder = [[AVAudioRecorder alloc] initWithURL:[NSURL URLWithString:kRecordFilePath] settings:recordSettings error:nil];
+    _audioRecorder.meteringEnabled = YES;
     [_audioRecorder prepareToRecord];
 }
 
 - (void)setupPlayer{
-    NSURL *url = [NSURL URLWithString:kRecordFilePath];
-    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    //----------------AVAudioPlayer----------------
+    NSError *playerError;
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:kRecordFilePath] error:&playerError];
+    if (_audioPlayer) {
+        _audioPlayer.volume = 1.0;
+    } else {
+        NSLog(@"Error: %@", [playerError localizedDescription]);
+    }
 }
 
 - (void)startRecord{
