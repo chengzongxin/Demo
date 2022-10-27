4c8f9e6381cff9dbb2b74ad13442d4a31b03d6b2
程宗鑫 <8876142@qq.com>
2022-10-27 23:26:19 +0800

    不规则图像

---
+++ Demo/HomeSection/IrregularViewController.h
@@ -0,0 +1,16 @@
+//
+//  IrregularViewController.h
+//  Demo
+//
+//  Created by 程宗鑫 on 2022/10/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IrregularViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
